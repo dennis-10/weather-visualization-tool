055b7c65a442426a6505813c590931fca3e3b078
Dennis Rodrigues <dennissantosr@gmail.com>
2022-10-13 01:47:05 -0300

    Using flask and removing unnecessary files and folders

---
+++ backend/src/__init__.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+from flask_swagger_ui import get_swaggerui_blueprint
+from alerta_rio_service import AlertaRioService
+from visualization_map import rio_map
+
+# Starting Flask app
+app = Flask(__name__)
+
+# Endpoint for main visualization
+@app.route('/')
+def index():
+    return render_template("index.html", map=rio_map._repr_html_())
+
+# Endpoint for getting alerta rio data
+@app.route("/data/")
+def get_data():
+    try:
+        seconds_interval = request.args.get("seconds_interval")
+
+        response = AlertaRioService.get_data(
+          request.args.get("start_time"),
+          request.args.get("end_time"),
+          int(seconds_interval),
+          request.args.get("station"))
+          
+        return response
+    except Exception as error:
+        raise error
+
+# Swagger DOCS
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+  SWAGGER_URL,
+  API_URL,
+  config={
+    'app_name': 'Weather Visualization Tool'
+  }
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+# Path to swagger docs
+@app.route('/static/<path:path>')
+def send_static(path):
+  return send_from_directory('static', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ backend/src/visualization_map.py
@@ -0,0 +1,72 @@
+import pandas as pd
+import geopandas as gpd
+import folium
+from shapely.geometry import Polygon
+
+# Reading and transforming Alerta Rio data
+df_estacoes = pd.read_csv('./src/static/estacoes_pluviometricas.csv')
+df_estacoes = df_estacoes.drop(columns=['Unnamed: 0'])
+df_estacoes.head()
+
+rio_map = folium.Map([-22.925778948753702, -43.489029909370046], zoom_start=10, tiles='cartodbpositron')
+colormap = ['magenta', 'red', 'orange', 'yellow']
+folium.LatLngPopup().add_to(rio_map)
+
+import warnings
+warnings.filterwarnings('ignore')
+
+lat_list = [-23.1339033365138, 
+             -23.0647349667651, 
+             -22.9955665970164, 
+             -22.9263982272677, 
+             -22.857229857519, 
+             -22.7880614877703, 
+             -22.7188931180216, -22.649724748272934]
+lon_list = [-43.8906028271505, 
+              -43.7697438637654, 
+              -43.6488849003802,
+              -43.5280259369951, 
+              -43.4071669736099,
+              -43.2863080102248,
+              -43.1654490468397, -43.04835145732227]
+              
+grid_cells = {}
+
+cell_idx = 1
+for j, _ in enumerate(lon_list):
+  if j + 1 == len(lon_list):
+    break
+  for i, _ in enumerate(lat_list):
+    if i + 1 == len(lat_list):
+      break
+
+    lon_ul, lat_ul = lon_list[j], lat_list[i]
+    lon_ur, lat_ur = lon_list[j+1], lat_list[i]
+    lon_lr, lat_lr = lon_list[j+1], lat_list[i+1]
+    lon_ll, lat_ll = lon_list[j], lat_list[i+1]
+    ul = (lon_ul, lat_ul)
+    ur = (lon_ur, lat_ur)
+    lr = (lon_lr, lat_lr)
+    ll = (lon_ll, lat_ll)
+
+    polygon_geom = Polygon([ul, ur, lr, ll])
+    grid_cells[cell_idx] = polygon_geom
+    cell_idx += 1
+
+    crs = {'init': 'epsg:4326'}
+    polygon = gpd.GeoDataFrame(index=[0], crs=crs, geometry=[polygon_geom])    
+    folium.GeoJson(polygon).add_to(rio_map)
+
+assert(49==len(grid_cells))
+
+lat = list(df_estacoes.Latitude)
+lon = list(df_estacoes.Longitude)
+
+for loc in zip(lat, lon):
+    folium.Circle(
+        location=loc,
+        radius=10,
+        fill=True,
+        color='red',
+        fill_opacity=0.7
+    ).add_to(rio_map)
