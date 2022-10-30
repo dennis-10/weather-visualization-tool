7eb819ee9e02fb568b48fd5056549588b60f24c3
raphadvl <raphael.rdoval@gmail.com>
2022-10-30 00:57:43 -0300

    creating chart animation

--- backend/src/visualization_line_chart.py
+++ backend/src/visualization_line_chart.py
@@ -15,3 +15,16 @@ y = station[choice]['volume']
 
 tfinal = max(x)
 x0 = 0
+
+fig, ax = plt.subplots(1, 1, figsize = (6, 3))
+
+
+def animate(i):
+    ax.cla() # clear the previous image
+    ax.plot(x[:i], y[:i]) # plot the line
+    ax.set_xlim([x0, tfinal]) # fix the x axis
+    ax.set_ylim([1.1*np.min(y), 1.1*np.max(y)]) # fix the y axis
+    ax.set_title(title)
+
+anim = animation.FuncAnimation(fig, animate, frames = len(x) + 1, interval = 350, blit = False)
+plt.show()
